a50c9c15868c76abf4cd017bb515ce3a75c81374
Pramod Maurya <118468618+pramodmaurya959@users.noreply.github.com>
2023-04-01 15:57:22 +0530

    Update Payment.jsx

--- src/Components/Payment/Payment.jsx
+++ src/Components/Payment/Payment.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { increaseStep } from "../../Redux/action";
 import styled from "styled-components";
-let data = ["1234 5678 9101 2345", "12/25", "123", "Shad Azam"];
+let data = ["1234 5678 9101 2345", "12/25", "123", "Pramod Maurya"];
 function Payment() {
   let [inp, setInp] = React.useState(data);
   let [color, setColor] = React.useState("#06A759");
